5574b6f957eb1f0a2c5cdc4819fe6c1eee6d4671
TshephoJoel <joelbosega@gmail.com>
2023-07-13 15:00:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Hello, printf
+ *
+ * Description: A program printed with printf, new line at the end
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
+}
